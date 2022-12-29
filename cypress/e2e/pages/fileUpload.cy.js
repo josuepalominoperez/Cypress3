2322ddea15748c970da9a0464d4199b69d0f6b2a
josuepalominoperez <oscar.palomino.perez@gmail.com>
2022-12-28 23:45:29 -0500

    Steps to configure fileUpload

--- cypress/e2e/pages/fileUpload.cy.js
+++ cypress/e2e/pages/fileUpload.cy.js
@@ -1,8 +1,24 @@
+/*Step 1: Installl plugin cypress-file-upload
+npm install --save-dev cypress-file-upload
+
+Step2: If we are using Typescript , ensure tsconfig.json contains
+"compilerOptions":{
+    "types":["cypress","cypress-file-upload"]}
+
+Step3: Add to cypress/support/commands.js
+import 'cypress-file-upload';
+
+Step 4: Check this commands.js is imported in cypress/support/index.js o su similar cypress/support/e2e.js
+import './commands';
+
+
+*/
 describe('file upload test', ()=>{
 
 it('file upload demo' , ()=>{
     cy.visit('https://demo.guru99.com/test/upload/')
-    cy.get('[id="uploadfile_0"]').attachFile('logoSpotify')
+    //The file should be in cypress/fixtures/
+    cy.get('[id="uploadfile_0"]').attachFile('logoSpotify.jpg')
     cy.get('#submitbutton').click()
 })
 
